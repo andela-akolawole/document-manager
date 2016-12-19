62e8f337da0e94f5b533dbabec68ff9a643027b3
andela-akolawole <aladekolawole15@yahoo.com>
2016-12-19 10:59:23 +0100

    fix travis build fail

--- config/config.js
+++ config/config.js
@@ -2,28 +2,29 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres",
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'document-manager',
+    host: '127.0.0.1',
+    dialect: 'postgres',
     seederStorage: 'sequelize',
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres",
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
     seederStorage: 'sequelize',
   },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres",
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
     seederStorage: 'sequelize',
-  }
-}
+  },
+};
+
